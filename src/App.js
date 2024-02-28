98ce3fc8cb54f7b4ef759998744c48d0e9f12dc4
VanshaSomani <vanshasomani0@gmail.com>
2024-02-28 11:55:12 +0530

    DarkMode - Remaining

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import { TextForm } from './components/TextForm';
+import { About } from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtil"/>
       <div className='container'>
-        <TextForm title="Enter Text Here"/>
+        {/* <TextForm title="Enter Text Here"/> */}
+        <About/>
       </div>
     </>
   );
